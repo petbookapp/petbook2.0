da17626bf0f7a52b8f00899d47a2c2dc062cc782
sakofchit <sakun.acharige@gmail.com>
2022-04-18 15:05:13 -0400

    fix urls + no more infinite loop

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,71 +1,102 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { database} from '../firebase'
-import { doc, getDoc } from "firebase/firestore";
+import { Modal, Button } from "react-bootstrap"
+import { useNavigate, useParams } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext'
+import { auth, database} from '../firebase'
+import { deleteDoc, doc, getDoc } from "firebase/firestore";
+import 'react-toastify/dist/ReactToastify.css';
 
 
-export default function Pet() {
-  const [pet, setPet] = useState("")
-  const [userId, setUID] = useState("")
+export default function PetInfo() {
+
   const [Name, setName] = useState("")
+  const [petName, setpetName] = useState("")
+  const [petGender, setGender] = useState("")
+  const [petWeight, setWeight] = useState("")
+  const [petBreed, setBreed] = useState("")
+  const [petType, setType] = useState("")
+  const [petAge, setAge] = useState("")
+  const [petPhoto, setPhoto] = useState("")
   const [Phone, setPhone] = useState("")
+  const [show, setShow] = useState(false)
   let { id } = useParams()
   
   useEffect(() => {
     getPet(id)
-    getUser(userId)
-  }, [id, userId]);
+    getUser()
+  }, [id]);
 
-  async function getPet(id) {
-    let petData = []
-    try {
-        const docRef = doc(database, "pets", id);
-        const snapshot = await getDoc(docRef)
-        petData.push({...snapshot.data()})
-        setUID(petData[0]["userAssociation"])
-        setPet(petData)
-      
-    } catch {
-      console.log("No such document!");
-    }
-  }
-
-  async function getUser(user) {
+  async function getUser() {
     let userData = []
     try {
-      const snapshot = await getDoc(user)
+      const docRef = doc(database, "users", auth.currentUser.uid);
+      const snapshot = await getDoc(docRef)
       
       userData.push({...snapshot.data()})
 
-      setName(userData[0]["display_name"])
-      setPhone(userData[0]["phone_number"])
+
+      setName(snapshot.data().display_name)
+      setPhone(snapshot.data().phone_number)
       
     } catch {
       console.log("No such document!");
     }
   }
 
+  async function getPet(id) {
+    
+    try {
+      const docRef = doc(database, "pets", id);
+      const snapshot = await getDoc(docRef)
+      console.log(snapshot.data().petName)
+      
+      setpetName(snapshot.data().petName);
+      setGender(snapshot.data().petGender);
+      setWeight(snapshot.data().petWeight);
+      setType(snapshot.data().petType);
+      setBreed(snapshot.data().petBreed);
+      setAge(snapshot.data().petAge);
+      setPhoto(snapshot.data().petPhoto);
+      
+      
+    } catch {
+      console.log("No such document!");
+    }
+  }
+
+
+
   return (
     <>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-      <div className="nicebackground">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossOrigin="anonymous" />
+      <div class="container nicebackground">
           <h2 style={{ fontSize: 25 }} className="text-center mb-4">
                 <img className="logo center-margin4" src="/logo.png" alt="logo"/>
             </h2>
-          {Object.keys(pet).map((key) => (
-              <>
                 <div class="team-single center-margin3">
                     <div class="row">
                         <div class="col-lg-4 col-md-5 xs-margin-30px-bottom">
                             <div class="team-single-img">
-                                <img src={pet[key]["petPhoto"]} alt="MyPet"/>
+                                <img src={petPhoto} alt="MyPet"/>
                             </div>
                         </div>
                         <div class="col-lg-8 col-md-7">
                             <div class="team-single-text padding-50px-left sm-no-padding-left">
                                 <div class="contact-info-section margin-40px-tb">
                                     <ul class="list-style9 no-margin">
+                                    <li>
+                                    <div class="row">
+                                        <div class="col-md-5 col-5">
+                                          <i class="fas fa-solid fa-trash text-black"></i>
+                                          <strong class="margin-10px-left text-orange"><button onClick={openDelete}>Delete</button></strong>
+                                        </div>
+                                        <div class="col-md-7 col-7">
+                                          <i class="fas fa-solid fa-pen text-blue"></i>
+                                          <strong class="margin-10px-left text-orange"><button2 onClick={showEdit} className='edit'>Edit</button2></strong>
+                                        </div>
+                                    </div>
 
+                                    </li>
                                         <li>
 
                                             <div class="row">
@@ -74,7 +105,7 @@ export default function Pet() {
                                                     <strong class="margin-10px-left text-orange">Name:</strong>
                                                 </div>
                                                 <div class="col-md-7 col-7">
-                                                    <p>{pet[key]["petName"]} </p>
+                                                    <p>{petName} </p>
                                                 </div>
                                             </div>
 
@@ -87,7 +118,7 @@ export default function Pet() {
                                                     <strong class="margin-10px-left text-purple">Type:</strong>
                                                 </div>
                                                 <div class="col-md-7 col-7">
-                                                    <p>{pet[key]["petType"]}</p>
+                                                    <p>{petType}</p>
                                                 </div>
                                             </div>
 
@@ -100,19 +131,20 @@ export default function Pet() {
                                                     <strong class="margin-10px-left text-green">Breed:</strong>
                                                 </div>
                                                 <div class="col-md-7 col-7">
-                                                    <p>{pet[key]["petBreed"]}</p>
+                                                    <p>{petBreed}</p>
                                                 </div>
                                             </div>
 
                                         </li>
                                         <li>
+
                                             <div class="row">
                                                 <div class="col-md-5 col-5">
                                                     <i class="fas fa-file text-pink"></i>
                                                     <strong class="margin-10px-left text-pink">Age:</strong>
                                                 </div>
                                                 <div class="col-md-7 col-7">
-                                                    <p>{pet[key]["petAge"]}</p>
+                                                    <p>{petAge}</p>
                                                 </div>
                                             </div>
 
@@ -124,7 +156,7 @@ export default function Pet() {
                                                     <strong class="margin-10px-left text-lightred">Gender:</strong>
                                                 </div>
                                                 <div class="col-md-7 col-7">
-                                                    <p>{pet[key]["petGender"]}</p>
+                                                    <p>{petGender}</p>
                                                 </div>
                                             </div>
                                         </li>
@@ -135,7 +167,7 @@ export default function Pet() {
                                                     <strong class="margin-10px-left text-purple">Weight:</strong>
                                                 </div>
                                                 <div class="col-md-7 col-7">
-                                                    <p>{pet[key]["petWeight"]}</p>
+                                                    <p>{petWeight}</p>
                                                 </div>
                                             </div>
                                         </li>
@@ -170,8 +202,6 @@ export default function Pet() {
                         <div class="col-md-12"></div>
                     </div>
                 </div>
-                </>
-            ))}
       </div>
     </>
   )
